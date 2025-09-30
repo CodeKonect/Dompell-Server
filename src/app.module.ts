c01b34c7164af1e7734ba058509081897b6493bf
george-asiedu <asiedug41@gmail.com>
2025-09-30 22:25:36 +0000

    config made global

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DbModule } from './db/db.module';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [DbModule, AuthenticationModule],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: '.env',
+        }),
+        DbModule,
+        AuthenticationModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
