45b0c21a18765bad5227a8d524b99488f2dd9ff7
george-asiedu <asiedug41@gmail.com>
2025-09-30 23:13:24 +0000

    login dto implemented

---
+++ src/authentication/dto/login.dto.ts
@@ -0,0 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+
+export class loginDto {
+    @ApiProperty({
+        example: 'alex@test.com',
+        required: true,
+    })
+    @IsString()
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @ApiProperty({
+        example: 'strongPass123',
+        required: true,
+    })
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+}
