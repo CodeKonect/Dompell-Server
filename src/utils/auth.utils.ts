9c62572ffca5e45d118b218d0e07605ec6f48a13
george-asiedu <asiedug41@gmail.com>
2025-09-30 22:57:23 +0000

    login token implemented to return access and refresh tokens

--- src/utils/auth.utils.ts
+++ src/utils/auth.utils.ts
@@ -1,4 +1,7 @@
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { AuthToken, Payload } from 'src/interfaces/auth.model';
 
 export const getPasswordHash = async (password: string) => {
     const salt = await bcrypt.genSalt(10);
@@ -14,4 +17,30 @@ export const verifyPassword = async (
 
 export const generateCode = (): string => {
     return Math.floor(100000 + Math.random() * 900000).toString();
+};
+
+export const loginToken = (
+    user: Payload,
+    jwt: JwtService,
+    config: ConfigService,
+) => {
+    const secret = config.get<string>('JWT_SECRET');
+    const accessDuration = config.get<string>('JWT_EXPIRATION');
+    const refreshDuration = config.get<string>('JWT_REFRESH_EXPIRES');
+
+    const accessToken = jwt.sign(
+        {
+            sub: user.id,
+            email: user.email,
+            role: user.role,
+            token: AuthToken.ACCESS_TOKEN,
+        },
+        { expiresIn: accessDuration, secret: secret },
+    );
+    const refreshToken = jwt.sign(
+        { sub: user.id, token: AuthToken.REFRESH_TOKEN },
+        { expiresIn: refreshDuration, secret: secret },
+    );
+
+    return { accessToken, refreshToken };
 };
