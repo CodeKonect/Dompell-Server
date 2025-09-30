eb6fd9a7f7ac73cf4041e96f0b52a4d29acfedba
george-asiedu <asiedug41@gmail.com>
2025-09-30 23:06:24 +0000

    login service method implemented to login a user

--- src/authentication/service/authentication.service.ts
+++ src/authentication/service/authentication.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UserRepository } from 'src/repository/user.repository';
 import { RegisterDto } from '../dto/register.dto';
-import { generateCode } from 'src/utils/auth.utils';
+import { generateCode, loginToken, verifyPassword } from 'src/utils/auth.utils';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { AccountStatus } from '@prisma/client';
+import { loginDto } from '../dto/login.dto';
 
 @Injectable()
 export class AuthenticationService {
@@ -30,5 +32,35 @@ export class AuthenticationService {
         );
 
         return { token };
+    }
+
+    protected async login(user: loginDto) {
+        const foundUser = await this.userRepo.getUserByEmail(user.email);
+        const validPassword = await verifyPassword(
+            user.password,
+            foundUser.password,
+        );
+        const isUserVerified = foundUser
+            ? foundUser.accountStatus === AccountStatus.VERIFIED
+            : false;
+
+        if (!isUserVerified)
+            throw new BadRequestException(
+                'Please verify your account before you can login',
+            );
+
+        if (!validPassword)
+            throw new BadRequestException('Invalid email or password');
+
+        const token = loginToken(foundUser, this.jwt, this.config);
+        return {
+            user: {
+                id: foundUser.id,
+                name: foundUser.name,
+                email: foundUser.email,
+                role: foundUser.role,
+            },
+            token,
+        };
     }
 }
