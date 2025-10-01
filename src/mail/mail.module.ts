ea817c2229b0cf7217058307d8a98c1cc3ac827c
george-asiedu <asiedug41@gmail.com>
2025-10-01 07:46:58 +0000

    nodemailer configured using the transport service

---
+++ src/mail/mail.module.ts
@@ -0,0 +1,42 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+
+@Module({
+  imports: [
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        transport: {
+          host: config.get<string>('MAIL_HOST'),
+          port: config.get<number>('MAIL_PORT'),
+          secure: false,
+          auth: {
+            user: config.get<string>('MAIL_USER'),
+            pass: config.get<string>('MAIL_PASSWORD'),
+          },
+          tls: {
+            rejectUnauthorized: false,
+          },
+          pool: true,
+          maxConnections: 5,
+          maxMessages: 100,
+          rateLimit: 20,
+        },
+        defaults: {
+          from: `"No Reply" <${config.get<string>('MAIL_FROM')}>`,
+        },
+        template: {
+          dir: process.cwd() + '/src/mail/templates',
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class MailModule {}
