e9e4089bf0459d698e887d4cd190bc67c53a44a8
george-asiedu <asiedug41@gmail.com>
2025-10-01 08:27:09 +0000

    verify and forgot email templated added

--- src/mail/service/mail.service.ts
+++ src/mail/service/mail.service.ts
@@ -1,5 +1,8 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable, Logger } from '@nestjs/common';
+import { ForgetEmail, VerifyEmail } from 'src/interfaces/auth.model';
+import { verifyEmailTemplate } from '../template/auth/verify_email.template';
+import { forgotPasswordTemplate } from '../template/auth/forgot_password.template';
 
 @Injectable()
 export class MailService {
@@ -7,40 +10,36 @@ export class MailService {
 
   constructor(private mailer: MailerService) {}
 
-  public async sendVerificationEmail(email: string, code: string) {
+  public async sendVerificationEmail(data: VerifyEmail) {
     try {
       await this.mailer.sendMail({
-        to: email,
+        to: data.email,
         subject: 'Verify your account',
-        html: '',
+        html: verifyEmailTemplate(data.name, data.code),
       });
-      this.logger.log(`Email sent to ${email}`);
+      this.logger.log(`Email sent to ${data.email}`);
     } catch (err: unknown) {
       const error = err as Error;
       this.logger.error(
-        `Failed to send email to ${email}: ${error.message}`,
+        `Failed to send email to ${data.email}: ${error.message}`,
         error.stack,
       );
       return false;
     }
   }
 
-  public async sendForgotPasswordEmail(
-    email: string,
-    token: string,
-    name: string,
-  ) {
+  public async sendForgotPasswordEmail(data: ForgetEmail) {
     try {
       await this.mailer.sendMail({
-        to: email,
+        to: data.email,
         subject: 'Forget password',
-        html: '',
+        html: forgotPasswordTemplate(data.name, data.token),
       });
-      this.logger.log(`Email sent to ${email}`);
+      this.logger.log(`Email sent to ${data.email}`);
     } catch (error: unknown) {
       const err = error as Error;
       this.logger.error(
-        `Failed to send email to ${email}: ${err.message}`,
+        `Failed to send email to ${data.email}: ${err.message}`,
         err.stack,
       );
       return false;
