3cd6d140e928f94d5b2fc42e4ffdac332756b59d
george-asiedu <asiedug41@gmail.com>
2025-10-04 17:12:40 +0000

    module dependencies defined in auth module

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,7 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationService } from './service/authentication.service';
+import { AuthenticationController } from './controller/authentication.controller';
+import { UserRepository } from 'src/repository/user.repository';
+import { MailModule } from 'src/mail/mail.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [AuthenticationService],
+  imports: [
+    MailModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: config.get<string>('JWT_EXPIRATION', '30m'),
+        },
+      }),
+    }),
+  ],
+  providers: [AuthenticationService, UserRepository],
+  controllers: [AuthenticationController],
+  exports: [JwtModule],
 })
 export class AuthenticationModule {}
