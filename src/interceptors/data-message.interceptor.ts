6973e4a2afea3def9808c768763c519f66c18a80
george-asiedu <asiedug41@gmail.com>
2025-10-04 17:17:30 +0000

    data message interceptor implemented

---
+++ src/interceptors/data-message.interceptor.ts
@@ -0,0 +1,36 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { Response as ExpressResponse } from 'express';
+import { map } from 'rxjs/operators';
+import { Data } from 'src/interfaces/auth.model';
+
+@Injectable()
+export class DataMessageInterceptor<T> implements NestInterceptor<T, Data<T>> {
+  constructor(private readonly reflector: Reflector) {}
+
+  public intercept(
+    _context: ExecutionContext,
+    next: CallHandler<T>,
+  ): Observable<Data<T>> | Promise<Observable<Data<T>>> {
+    const message = this.reflector.get<string>(
+      'message',
+      _context.getHandler(),
+    );
+    const ctx = _context.switchToHttp();
+    const response = ctx.getResponse<ExpressResponse>();
+
+    return next.handle().pipe(
+      map((data: T) => ({
+        status: response.statusCode,
+        message,
+        data,
+      })),
+    );
+  }
+}
