842483303636cac777024d874508195e6213d036
george-asiedu <asiedug41@gmail.com>
2025-10-06 14:48:08 +0000

    programs service added to providers

--- src/organization/organization.module.ts
+++ src/organization/organization.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { OrganizationService } from './service/organization/organization.service';
+import { ProgramsService } from './service/programs/programs.service';
 
-@Module({})
+@Module({
+  providers: [OrganizationService, ProgramsService],
+})
 export class OrganizationModule {}
