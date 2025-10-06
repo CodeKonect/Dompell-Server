71e961291211ea6381ea9fa7820cf40f9c87af23
george-asiedu <asiedug41@gmail.com>
2025-10-06 14:49:17 +0000

    organization service methods implemented

---
+++ src/organization/service/organization/organization.service.ts
@@ -0,0 +1,58 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { OrganizationRepository } from 'src/repository/organization.repository';
+import { OrganizationProfileDto } from '../dto/create_update.dto';
+
+@Injectable()
+export class OrganizationService extends OrganizationRepository {
+  public async getOrganizationProfile(id: string) {
+    const organization = await this.getOrganizationById(id);
+    if (!organization) {
+      throw new NotFoundException(`Organization with ID ${id} not found`);
+    }
+
+    return organization;
+  }
+
+  public async getOrganizationByUserId(userId: string) {
+    const organization = await this.getOrganizationUser(userId);
+
+    if (!organization) {
+      throw new NotFoundException(`No organization found for user ${userId}`);
+    }
+
+    return organization;
+  }
+
+  public async getAllOrganizations() {
+    return await this.getOrganizationUsers();
+  }
+
+  public async createOrUpdateOrganization(
+    userId: string,
+    data: OrganizationProfileDto,
+  ) {
+    return await this.createOrganization(userId, data);
+  }
+
+  public async deleteOrganization(id: string) {
+    const organization = await this.getOrganizationById(id);
+
+    if (!organization) {
+      throw new NotFoundException(`Organization with ID ${id} not found`);
+    }
+
+    return await super.deleteOrganization(id);
+  }
+
+  public async getOrganizationTrainingPrograms(orgId: string) {
+    const programs = await this.getOrgTrainingPrograms(orgId);
+
+    if (!programs.length) {
+      throw new NotFoundException(
+        `No training programs found for organization ${orgId}`,
+      );
+    }
+
+    return programs;
+  }
+}
