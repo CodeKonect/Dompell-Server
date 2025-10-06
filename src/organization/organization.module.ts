63b3d77f52b50d7fd61811816ffaf89b0a7687f8
george-asiedu <asiedug41@gmail.com>
2025-10-06 15:16:43 +0000

    controllers imported

--- src/organization/organization.module.ts
+++ src/organization/organization.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrganizationService } from './service/organization/organization.service';
 import { ProgramsService } from './service/programs/programs.service';
+import { ProgramsController } from './controller/programs/programs.controller';
+import { OrganizationController } from './controller/organization/organization.controller';
 
 @Module({
   providers: [OrganizationService, ProgramsService],
+  controllers: [OrganizationController, ProgramsController],
 })
 export class OrganizationModule {}
