214da3315b545ec8c6f00a35ca3b7e59c013c61c
george-asiedu <asiedug41@gmail.com>
2025-10-18 17:49:01 +0000

    trainee profile repository crud operations implemented

--- src/repository/trainee.repository.ts
+++ src/repository/trainee.repository.ts
@@ -1,26 +1,64 @@
+import { Prisma } from '@prisma/client';
 import { DbConnectService } from 'src/db/db-connect.service';
+
+export const traineeProfileInclude =
+  Prisma.validator<Prisma.TraineeProfileInclude>()({
+    educations: true,
+    experiences: true,
+    certifications: true,
+    portfolioProjects: true,
+    skills: true,
+    user: {
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+      },
+    },
+    enrollments: true,
+  });
 
 export class TraineeRepository extends DbConnectService {
   public async findByID(id: string) {
-    const trainee = this.traineeProfile.findUnique({
+    const trainee = await this.traineeProfile.findUnique({
       where: { id },
-      include: {
-        educations: true,
-        experiences: true,
-        certifications: true,
-        portfolioProjects: true,
-        skills: true,
-        user: {
-          select: {
-            id: true,
-            name: true,
-            email: true,
-            role: true,
-          },
-        },
-      },
+      include: traineeProfileInclude,
     });
 
     return trainee;
+  }
+
+  public async findByUserID(userId: string) {
+    return this.traineeProfile.findUnique({
+      where: { userId },
+      include: traineeProfileInclude,
+    });
+  }
+
+  public async create(data: Prisma.TraineeProfileCreateInput) {
+    return this.traineeProfile.create({
+      data,
+    });
+  }
+
+  public async update(id: string, data: Prisma.TraineeProfileUpdateInput) {
+    return this.traineeProfile.update({
+      where: { id },
+      data,
+    });
+  }
+
+  public async updateProfileCompletion(id: string, isComplete: boolean) {
+    return this.traineeProfile.update({
+      where: { id },
+      data: { profileComplete: isComplete },
+    });
+  }
+
+  public async delete(id: string) {
+    return this.traineeProfile.delete({
+      where: { id },
+    });
   }
 }
