cba06c24e8d4caea87c5d11e0d1205bbb4d58dfa
george-asiedu <asiedug41@gmail.com>
2025-10-18 20:18:43 +0000

    imports and providers added

--- src/trainee/trainee.module.ts
+++ src/trainee/trainee.module.ts
@@ -4,8 +4,27 @@ import { EducationService } from './service/education/education.service';
 import { PortfolioService } from './service/portfolio/portfolio.service';
 import { SkillsService } from './service/skills/skills.service';
 import { ExperienceService } from './service/experience/experience.service';
+import { TraineeController } from './controller/trainee/trainee.controller';
+import { SkillsController } from './controller/skills/skills.controller';
+import { S3Service } from 'src/s3-bucket/service/s3.service';
+import { TraineeRepository } from 'src/repository/trainee.repository';
+import { AuthGuard } from 'src/guards/auth/auth.guard';
+import { JwtModule } from '@nestjs/jwt';
+import { UserRepository } from 'src/repository/user.repository';
 
 @Module({
-  providers: [TraineeService, EducationService, PortfolioService, SkillsService, ExperienceService]
+  imports: [JwtModule],
+  providers: [
+    TraineeService,
+    EducationService,
+    PortfolioService,
+    SkillsService,
+    ExperienceService,
+    S3Service,
+    TraineeRepository,
+    AuthGuard,
+    UserRepository,
+  ],
+  controllers: [TraineeController, SkillsController],
 })
 export class TraineeModule {}
