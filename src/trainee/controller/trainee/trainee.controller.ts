dd4f40e4a3daf99835d950154450f9596a69f476
george-asiedu <asiedug41@gmail.com>
2025-10-18 20:19:16 +0000

    trainee controller methods implemented

---
+++ src/trainee/controller/trainee/trainee.controller.ts
@@ -0,0 +1,197 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Get,
+  Delete,
+  SetMetadata,
+  UseGuards,
+  UseInterceptors,
+  UsePipes,
+  ValidationPipe,
+  UploadedFiles,
+} from '@nestjs/common';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
+} from '@nestjs/swagger';
+import { Role } from '@prisma/client';
+import { AuthGuard } from 'src/guards/auth/auth.guard';
+import { Roles } from 'src/guards/role/role.decorator';
+import { RolesGuard } from 'src/guards/role/roles.guard';
+import { DataMessageInterceptor } from 'src/interceptors/data-message.interceptor';
+import { MessageInterceptor } from 'src/interceptors/message.interceptor';
+import {
+  unauthorizedResponse,
+  accessDeniedResponse,
+} from 'src/swagger/users.swagger';
+import { CreateTraineeProfileDto } from 'src/trainee/dto/trainee.dto';
+import { TraineeService } from 'src/trainee/service/trainee/trainee.service';
+
+@ApiTags('Trainee Profile')
+@UseGuards(AuthGuard, RolesGuard)
+@ApiBearerAuth()
+@Controller('trainee')
+export class TraineeController {
+  constructor(private ts: TraineeService) {}
+
+  @Post('create/:userId')
+  @Roles(Role.TRAINEE)
+  @HttpCode(201)
+  @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
+  @UseInterceptors(
+    DataMessageInterceptor,
+    FileFieldsInterceptor([
+      { name: 'cv', maxCount: 1 },
+      { name: 'avatar', maxCount: 1 },
+    ]),
+  )
+  @SetMetadata('message', 'Trainee Profile successful')
+  @ApiOperation({ summary: 'Create or update a trainee profile' })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'Trainee profile data',
+    schema: {
+      type: 'object',
+      properties: {
+        headline: { type: 'string' },
+        bio: { type: 'string' },
+        profilePictureUrl: { type: 'string' },
+        location: { type: 'string' },
+        cv: { type: 'string', format: 'binary' },
+        avatar: { type: 'string', format: 'binary' },
+      },
+    },
+  })
+  @ApiResponse({ status: 201, description: 'Success' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+    example: unauthorizedResponse,
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+    example: accessDeniedResponse,
+  })
+  async createTraineeProfile(
+    @Param('userId', ParseUUIDPipe) userId: string,
+    @Body() data: CreateTraineeProfileDto,
+    @UploadedFiles()
+    files: { cv?: Express.Multer.File[]; avatar?: Express.Multer.File[] },
+  ) {
+    const cv = files.cv?.[0];
+    const avatar = files.avatar?.[0];
+    return await this.ts.createProfile(data, userId, cv, avatar);
+  }
+
+  @Get(':id')
+  @Roles(Role.ADMIN, Role.INSTITUTION, Role.TRAINEE, Role.EMPLOYER)
+  @HttpCode(200)
+  @UseInterceptors(DataMessageInterceptor)
+  @SetMetadata('message', 'Fetched trainee profile successfully')
+  @ApiOperation({
+    summary: 'Get a trainee profile',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Success',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+    example: unauthorizedResponse,
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+    example: accessDeniedResponse,
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+    example: {
+      message: 'Trainee profile not found',
+    },
+  })
+  async getTraineeProfile(@Param('id', ParseUUIDPipe) id: string) {
+    return await this.ts.getProfile(id);
+  }
+
+  @Get(':userId')
+  @Roles(Role.ADMIN, Role.INSTITUTION, Role.TRAINEE, Role.EMPLOYER)
+  @HttpCode(200)
+  @UseInterceptors(DataMessageInterceptor)
+  @SetMetadata('message', 'Fetched trainee profile successfully')
+  @ApiOperation({
+    summary: 'Get a trainee profile',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Success',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+    example: unauthorizedResponse,
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+    example: accessDeniedResponse,
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+    example: {
+      message: 'Trainee profile not found',
+    },
+  })
+  async getTraineeProfileByUserId(
+    @Param('userId', ParseUUIDPipe) userId: string,
+  ) {
+    return await this.ts.getProfileByUserId(userId);
+  }
+
+  @Delete(':id')
+  @Roles(Role.ADMIN, Role.INSTITUTION)
+  @HttpCode(200)
+  @UseInterceptors(MessageInterceptor)
+  @SetMetadata('message', 'Deleted trainee profile successfully')
+  @ApiOperation({
+    summary: 'Delete a trainee profile',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Success',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+    example: unauthorizedResponse,
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+    example: accessDeniedResponse,
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+    example: {
+      message: 'Trainee profile not found',
+    },
+  })
+  async deleteTraineeProfile(@Param('id', ParseUUIDPipe) id: string) {
+    return await this.ts.deleteProfile(id);
+  }
+}
