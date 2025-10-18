160d255e183c48b10ea36639ce08acf3df699560
george-asiedu <asiedug41@gmail.com>
2025-10-18 19:09:46 +0000

    providers added to module

--- src/trainee/trainee.module.ts
+++ src/trainee/trainee.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TraineeService } from './service/trainee/trainee.service';
+import { EducationService } from './service/education/education.service';
+import { PortfolioService } from './service/portfolio/portfolio.service';
+import { SkillsService } from './service/skills/skills.service';
+import { ExperienceService } from './service/experience/experience.service';
 
-@Module({})
+@Module({
+  providers: [TraineeService, EducationService, PortfolioService, SkillsService, ExperienceService]
+})
 export class TraineeModule {}
