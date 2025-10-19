064c7625b59a7c9788163967a342dcad5c674c6d
george-asiedu <asiedug41@gmail.com>
2025-10-19 11:43:04 +0000

    unsued fils removed

--- src/trainee/trainee.module.ts
+++ src/trainee/trainee.module.ts
@@ -2,10 +2,8 @@ import { Module } from '@nestjs/common';
 import { TraineeService } from './service/trainee/trainee.service';
 import { EducationService } from './service/education/education.service';
 import { PortfolioService } from './service/portfolio/portfolio.service';
-import { SkillsService } from './service/skills/skills.service';
 import { ExperienceService } from './service/experience/experience.service';
 import { TraineeController } from './controller/trainee/trainee.controller';
-import { SkillsController } from './controller/skills/skills.controller';
 import { S3Service } from 'src/s3-bucket/service/s3.service';
 import { TraineeRepository } from 'src/repository/trainee.repository';
 import { AuthGuard } from 'src/guards/auth/auth.guard';
@@ -19,13 +17,12 @@ import { EducationController } from './controller/education/education.controller
     TraineeService,
     EducationService,
     PortfolioService,
-    SkillsService,
     ExperienceService,
     S3Service,
     TraineeRepository,
     AuthGuard,
     UserRepository,
   ],
-  controllers: [TraineeController, SkillsController, EducationController],
+  controllers: [TraineeController, EducationController],
 })
 export class TraineeModule {}
