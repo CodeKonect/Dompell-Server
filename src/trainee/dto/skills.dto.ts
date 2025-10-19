9af40344403d7c7f5d49288d2aaa07bd83940a2b
george-asiedu <asiedug41@gmail.com>
2025-10-19 11:25:08 +0000

    skills dto modified

--- src/trainee/dto/skills.dto.ts
+++ src/trainee/dto/skills.dto.ts
@@ -1,7 +1,7 @@
 import { IsString, IsOptional } from 'class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
-export class CreatePortfolioDto {
+export class AddSkillDto {
   @ApiProperty()
   @IsString()
   name: string;
