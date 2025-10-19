5b0784bdd83776d96823df0bcaf500221406032e
george-asiedu <asiedug41@gmail.com>
2025-10-19 09:43:35 +0000

    education repository implemented to define crud logic

---
+++ src/repository/education.repository.ts
@@ -0,0 +1,55 @@
+import { DbConnectService } from '../db/db-connect.service';
+import {
+  CreateEducationDto,
+  UpdateEducationDto,
+} from '../trainee/dto/education.dto';
+
+export class EducationRepository extends DbConnectService {
+  public async findOne(id: string) {
+    return this.education.findUnique({
+      where: { id },
+      include: {
+        traineeProfile: true,
+      },
+    });
+  }
+
+  public async create(education: CreateEducationDto, traineeProfileId: string) {
+    return this.education.create({
+      data: {
+        ...education,
+        traineeProfile: {
+          connect: { id: traineeProfileId },
+        },
+      },
+    });
+  }
+
+  public async update(
+    id: string,
+    data: UpdateEducationDto,
+    traineeProfileId: string,
+  ) {
+    return this.education.update({
+      where: { id },
+      data: {
+        ...data,
+        traineeProfile: {
+          connect: { id: traineeProfileId },
+        },
+      },
+    });
+  }
+
+  public async findByTraineeProfileId(traineeProfileId: string) {
+    return this.education.findMany({
+      where: { traineeProfileId },
+      orderBy: { startDate: 'desc' },
+      include: { traineeProfile: true },
+    });
+  }
+
+  public async delete(id: string) {
+    return this.education.delete({ where: { id } });
+  }
+}
