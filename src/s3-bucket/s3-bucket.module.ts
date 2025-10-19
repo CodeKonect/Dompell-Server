f09927e21ea98675e656e02837dae5d98692c865
george-asiedu <asiedug41@gmail.com>
2025-10-19 12:57:34 +0000

    controller deleted

--- src/s3-bucket/s3-bucket.module.ts
+++ src/s3-bucket/s3-bucket.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { S3Service } from './service/s3.service';
-import { S3Controller } from './controller/s3.controller';
 
 @Module({
   providers: [S3Service],
-  controllers: [S3Controller],
   exports: [S3Service],
 })
 export class S3BucketModule {}
